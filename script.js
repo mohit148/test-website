9eeedcb43a9bf1df25d1b3141e8f163136f29af6
Mohit <116991297+mohit148@users.noreply.github.com>
2023-12-24 15:02:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+/* var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+
+    if (currentScrollPos > 20) {
+        // Adjust the top position to hide the navigation bar
+        document.querySelector(".top").style.top = "-80px"; // Adjust the height of your navigation bar accordingly
+    } else {
+        // Reset the top position to show the navigation bar
+        document.querySelector(".top").style.top = "0";
+    }
+
+    prevScrollpos = currentScrollPos;
+} */
